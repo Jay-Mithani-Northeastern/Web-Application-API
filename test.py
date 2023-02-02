6674954f30d4f87ca48c3d3e3caaa466f91374af
Jay Mithani <114779988+jaymithani1998@users.noreply.github.com>
2023-02-01 20:27:12 -0500

    Updated test cases

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 import unittest
-from app import app
-
+from app import validation
 
 class WebAppTest(unittest.TestCase):
 
     #check for response 200
-    def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get("/healthz")
-        statuscode = response.status_code
-        self.assertEqual(statuscode,200)
+    def test_validation_function(self):
+        self.assertEqual(validation("abc", "xyz", "abc@gmail.com", "kljh"), "")
+        self.assertEqual(validation("abc", "xyz", "abcgail.com", "kljh"), 'Username should contain email address in correction format (example: demo@domain.com)')
+
+    
 
 if __name__ == "__main__":
     unittest.main()
