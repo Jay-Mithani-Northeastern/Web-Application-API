1541282f20a6795cad227579b9236a119562943f
Jay Mithani <114779988+jaymithani1998@users.noreply.github.com>
2023-02-08 18:50:22 -0500

    Changes in test file

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import unittest
-from app import validation, app
+from util import validations
 
 class WebAppTest(unittest.TestCase):
 
     #check for response 200
     def test_validation_function(self):
-        self.assertEqual(validation("abc", "xyz", "abc@gmail.com", "kljh"), "")
-        self.assertEqual(validation("abc", "xyz", "abcgail.com", "kljh"), 'Username should contain email address in correction format (example: demo@domain.com)')
+        data1 = {
+            "first_name" : "test",
+            "last_name" : "demo",
+            "username" :  "abc@gmail.com",
+            "password" : "add"
+        }
+        data2 = {
+            "first_name" : "test",
+            "last_name" : "demo",
+            "username" :  "abcgmail.com",
+            "password" : "add"
+        }
+        self.assertEqual(validations.Validation.isUserValid(data1), "")
+        self.assertEqual(validations.Validation.isUserValid(data2), "Username should contain email address in correction format (example: demo@domain.com)")
 
     
 
