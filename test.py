1e61c3cee599d7548fff04ce83564a4d08babba5
Jay Mithani <114779988+jaymithani1998@users.noreply.github.com>
2023-02-08 18:37:13 -0500

    Changes in Test File

--- test.py
+++ test.py
@@ -8,11 +8,6 @@ class WebAppTest(unittest.TestCase):
         self.assertEqual(validation("abc", "xyz", "abc@gmail.com", "kljh"), "")
         self.assertEqual(validation("abc", "xyz", "abcgail.com", "kljh"), 'Username should contain email address in correction format (example: demo@domain.com)')
 
-    def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get("/healthz")
-        statuscode = response.status_code
-        self.assertEqual(statuscode,200)
     
 
 if __name__ == "__main__":
